cedb6dcb6dee8da888d4134478b814aa8ebeba7d
Keith R <khr8771@gmail.com>
2020-05-01 22:38:00 -0700

    Added birthday + age calculation to swimmer model

--- data/models.py
+++ data/models.py
@@ -1,13 +1,23 @@
+from datetime import datetime, date
+
 from django.conf import settings
 from django.db import models
 
-class Swimmer(models.Model):
+
+def getAge(date):
+    pass
+
+
+class Club(models.Model):
     name = models.CharField(max_length=75)
-    age = models.IntegerField()
-    # club = reference to club class
+    city = models.CharField(max_length=35)
+    pool = models.CharField(max_length=75)
+    # coach = refer to a coach class
+    # swimmers = +1 everytime swimmer is added to club
 
     def __str__(self):
         return self.name
+
 
 class Coach(models.Model):
     name = models.CharField(max_length=75)
@@ -16,12 +26,35 @@ class Coach(models.Model):
     def __str__(self):
         return self.name
 
-class Club(models.Model):
+
+class Swimmer(models.Model):
+    GENDERS = (
+        ('CH', 'Choose a Gender'),
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other')
+    )
     name = models.CharField(max_length=75)
-    city = models.CharField(max_length=35)
-    pool = models.CharField(max_length=75)
-    # coach = refer to a coach class
-    # swimmers = +1 everytime swimmer is added to club
+    birthday = models.DateField(verbose_name="Birthday", null=True)
+    age = models.IntegerField(default=0)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE, default='')
+    gender = models.CharField(
+        max_length=2,
+        choices=GENDERS,
+        default='CH'
+    )
+
+    @property
+    def get_age(self):
+        today = date.today()
+        age = today.year - self.birthday.year - ((today.month, today.day) < (self.birthday.month, self.birthday.day))
+        return int(age)
+
+
+    def save(self, *args, **kwargs):
+        self.age = self.get_age
+        super(Swimmer, self).save(*args, **kwargs)
+
 
     def __str__(self):
         return self.name
