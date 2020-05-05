e42057ee61c284a3e7007a6d0d9cd69536124974
Keith R <khr8771@gmail.com>
2020-05-05 14:08:23 -0700

    Added swimmer details to main page

--- data/urls.py
+++ data/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.data_list, name='data_list')
+    path('', views.data_list, name='data_list'),
+    path('swimmer/<int:pk>', views.swimmer_detail, name='swimmer_detail')
 ]
--- data/views.py
+++ data/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Swimmer
 
 
@@ -6,3 +6,6 @@ def data_list(request):
     swimmers = Swimmer.objects.all()
     return render(request, 'data/data_list.html', {'swimmers': swimmers})
 
+def swimmer_detail(request, pk):
+    swimmer = get_object_or_404(Swimmer, pk=pk)
+    return render(request, 'data/swimmer_detail.html', {'swimmer': swimmer})
