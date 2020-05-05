384a90d9aec92ecd36ff55c48cc9d328b9f83929
Keith R <khr8771@gmail.com>
2020-05-05 12:27:44 -0700

    Modified templates to display swimmers from database

--- data/views.py
+++ data/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Swimmer
 
 
 def data_list(request):
-    return render(request, 'data/data_list.html', {})
+    swimmers = Swimmer.objects.all()
+    return render(request, 'data/data_list.html', {'swimmers': swimmers})
 
