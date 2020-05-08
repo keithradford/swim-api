d22ecedc5f9036e0386dde2bb51953358f6300bf
Keith R <khr8771@gmail.com>
2020-05-07 20:54:49 -0700

    Modify settings

--- swim/settings.py
+++ swim/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SWIM_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_VALUE', False)
+# DEBUG = os.environ.get('DEBUG_VALUE', False)
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
